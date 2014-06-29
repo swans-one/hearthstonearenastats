36e55f5bbfafee13bc445c89a0915d8ffbdf1ee5
Erik Swanson <theerikswanson@gmail.com>
2014-06-29 11:54:50 -0400

    Super simple profile page.

--- hearthstonearenastats/urls.py
+++ hearthstonearenastats/urls.py
@@ -6,7 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    (r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^account/login/', 'django.contrib.auth.views.login'),
+    url(r'^account/login/', 'django.contrib.auth.views.login'),
+    url(r'^account/profile/', TemplateView.as_view(template_name='account/profile.html')),
 )
