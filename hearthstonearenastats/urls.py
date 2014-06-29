8ce910d4e5c23b513de1ea6dd9f9cd143a1c2feb
Erik Swanson <theerikswanson@gmail.com>
2014-06-29 12:03:54 -0400

    Basic logout page.

--- hearthstonearenastats/urls.py
+++ hearthstonearenastats/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns(
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/login/', 'django.contrib.auth.views.login'),
+    url(r'^account/logout', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^account/profile/', TemplateView.as_view(template_name='account/profile.html')),
 )
