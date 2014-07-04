b169eb816c8f7b42764b6e2ab42b9cd15de1090d
Erik Swanson <theerikswanson@gmail.com>
2014-07-04 16:02:25 -0400

    Revise url router formatting, include account urls.

--- hearthstonearenastats/urls.py
+++ hearthstonearenastats/urls.py
@@ -6,16 +6,24 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^account/login/', 'django.contrib.auth.views.login'),
-    url(
-        r'^account/logout',
-        'django.contrib.auth.views.logout',
-        {'next_page': '/'}
+    url(r'^$',
+        TemplateView.as_view(template_name='index.html'),
     ),
-    url(
-        r'^account/profile/',
-        TemplateView.as_view(template_name='account/profile.html')
+    url(r'^admin/',
+        include(admin.site.urls),
+    ),
+    url(r'^account/login/',
+        'django.contrib.auth.views.login',
+    ),
+    url(r'^account/logout',
+        'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+    ),
+    url(r'^account/',
+        include('hearthstonearenastats.app.account.urls'),
+        namespace='account',
+    ),
+    url(r'^account/profile/',
+        TemplateView.as_view(template_name='account/profile.html'),
     ),
 )
