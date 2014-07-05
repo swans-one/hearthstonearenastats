a7a31ada4a67fbd9181e1828a24c76ffaba0402d
Erik Swanson <theerikswanson@gmail.com>
2014-07-05 15:25:35 -0400

    remove trailing whitespace.

--- hearthstonearenastats/urls.py
+++ hearthstonearenastats/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns(
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/', 'django.contrib.auth.views.logout', 
+    url(r'^logout/', 'django.contrib.auth.views.logout',
         {'next_page': '/'}, name='logout'),
 
     url(r'^account/',
