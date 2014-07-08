beb0d52fac305dfce48bd73dea6e4240f19d2089
Erik Swanson <theerikswanson@gmail.com>
2014-07-07 22:33:00 -0400

    Accounts are now auto-created with users.

--- hearthstonearenastats/app/account/models.py
+++ hearthstonearenastats/app/account/models.py
@@ -1,8 +1,19 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from timezone_field import TimeZoneField
 
 
 class Account(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(User)
     timezone = TimeZoneField(default='US/Eastern')
+
+    def __unicode__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_account(sender, instance, created, **kwargs):
+    if created:
+        account, created = Account.objects.get_or_create(user=instance)
